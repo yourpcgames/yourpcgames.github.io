caddf0091ad145daed8be9a187a8ad4264c61852
yourpcgames <muazamshah9610@gmail.com>
2025-03-14 12:22:42 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleScreenshots() {
+    var screenshots = document.getElementById("screenshots");
+    if (screenshots.style.display === "none") {
+        screenshots.style.display = "block";
+    } else {
+        screenshots.style.display = "none";
+    }
+}
