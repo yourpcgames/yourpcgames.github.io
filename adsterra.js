0079a696976df99ad6c4ee66c280bf3204ebc61f
Yourpcgames <muazamshah9610@gmail.com>
2025-03-15 15:12:23 +0500

    Create adsterra.js

---
+++ adsterra.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '787948dde80adc2501c819c5f7a0ce19',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.highperformanceformat.com/787948dde80adc2501c819c5f7a0ce19/invoke.js"></script>
