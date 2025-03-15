b04d2023580c3aea4c376b5e03218dd80e84b528
yourpcgames <muazamshah9610@gmail.com>
2025-03-15 06:00:42 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleScreenshots() {
-    var screenshots = document.getElementById("screenshots");
-    if (screenshots.style.display === "none") {
-        screenshots.style.display = "block";
-    } else {
-        screenshots.style.display = "none";
-    }
-}
