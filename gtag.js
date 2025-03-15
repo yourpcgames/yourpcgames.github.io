a2b60256628204d722dc0bd94240a8e9b6246005
Yourpcgames <muazamshah9610@gmail.com>
2025-03-15 12:31:14 +0500

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3C0E81DX5G"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3C0E81DX5G');
+</script>
