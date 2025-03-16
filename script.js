1e8e6ea1c4f580acff5b2aeb3eb0b8d1ec36a2b0
Yourpcgames <muazamshah9610@gmail.com>
2025-03-16 06:01:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+<!-- import the webpage's javascript file -->
+	 
+<script type='text/javascript' src='//pl26123463.effectiveratecpm.com/d8/71/e9/d871e9d5735ede95cca7a01a9b5ec8ba.js'></script>
+
+
 function searchGame() {
     let input = document.getElementById('searchBar').value.toLowerCase();
     let games = document.querySelectorAll('.game-item');
